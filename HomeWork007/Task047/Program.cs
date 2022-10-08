c98fc47311f4e260b196ba69973666848d3d9b26
Belov Igor <grot.ik@mail.ru>
2022-10-08 12:47:53 +0200

    HomeWork007

---
+++ HomeWork007/Task047/Program.cs
@@ -0,0 +1,12 @@
+﻿double[,] a = new double[3, 4];
+
+Random random = new Random();
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        a[i, j] = random.NextDouble() * 10;
+        Console.Write("{0,6:F2}", a[i, j]);
+    }
+    Console.WriteLine();
+}
---
+++ HomeWork007/Task052/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите количество строк:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[n, m];
+int[] sum = new int[m];
+Random ran = new Random();
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        arr[i, j] = ran.Next(0, 100);
+
+        Console.Write(arr[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        sum[i] += arr[j, i];
+    }
+}
+
+for (int i = 0; i < m; i++)
+{
+    Console.Write((sum[i] / n) + " ");
+}
